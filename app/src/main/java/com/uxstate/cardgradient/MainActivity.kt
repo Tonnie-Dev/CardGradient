8d9b8876001c29a876315d3044c5f366924cd152
Antony <vontonnie@gmail.com>
2022-01-18 20:21:02 +0300

    Add ImageCardComposable

--- app/src/main/java/com/uxstate/cardgradient/MainActivity.kt
+++ app/src/main/java/com/uxstate/cardgradient/MainActivity.kt
@@ -3,10 +3,15 @@ package com.uxstate.cardgradient
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.tooling.preview.Preview
 import com.uxstate.cardgradient.ui.theme.CardGradientTheme
 
@@ -22,3 +27,10 @@ class MainActivity : ComponentActivity() {
     }
 
 
+@Composable
+fun ImageCardComposable(painter: Painter, description:String, title:String) {
+
+
+   
+
+}
