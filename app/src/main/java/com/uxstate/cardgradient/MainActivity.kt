cbc638f56af4879b017523e09feb80ed5ad4bcfc
Antony <vontonnie@gmail.com>
2022-01-18 20:25:43 +0300

    Apply layer 2 - Gradient

--- app/src/main/java/com/uxstate/cardgradient/MainActivity.kt
+++ app/src/main/java/com/uxstate/cardgradient/MainActivity.kt
@@ -4,15 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.uxstate.cardgradient.ui.theme.CardGradientTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,21 +22,36 @@ class MainActivity : ComponentActivity() {
         setContent {
             CardGradientTheme {
 
-                }
             }
         }
     }
+}
 
 
 @Composable
-fun ImageCardComposable(painter: Painter, description:String, title:String) {
+fun ImageCardComposable(painter: Painter, description: String, title: String) {
 
 
+    Box(modifier = Modifier.width(250.dp)) {
 
-    Box(modifier = Modifier.fillMaxSize()){
-
-        //Layer 1
+        //Layer 1 - Image
         Image(painter = painter, contentDescription = description)
+
+        //Layer 2 - Gradient - wrap in a box to apply modifier
+
+        Box(
+            modifier = Modifier
+                    .fillMaxSize()
+                    .background(
+                        brush = Brush.verticalGradient(
+                          colors=  listOf(
+                                Color.Transparent,
+                                Color.Black
+                            ),
+                            startY = 300f
+                        )
+                    )
+        )
     }
 
 }
