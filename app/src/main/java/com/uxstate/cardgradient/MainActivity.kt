9d3ceee770373aeb981f7d3c667684f79d7e0f95
Antony <vontonnie@gmail.com>
2022-01-18 20:35:38 +0300

    Wrap layers in a card

--- app/src/main/java/com/uxstate/cardgradient/MainActivity.kt
+++ app/src/main/java/com/uxstate/cardgradient/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.width
+import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -32,7 +33,10 @@ class MainActivity : ComponentActivity() {
             val title = "Kermit Playing in the Snow"
             val description = "Kermit Playing in the Snow"
 
-            
+
+            //call ImageCardComposable
+            ImageCardComposable(painter = painter, description = description, title = title)
+
         }
     }
 }
@@ -42,35 +46,39 @@ class MainActivity : ComponentActivity() {
 fun ImageCardComposable(painter: Painter, description: String, title: String) {
 
 
-    Box(modifier = Modifier.width(250.dp)) {
+    Card(elevation = 5.dp,modifier = Modifier.width(250.dp)) {
+        Box() {
 
-        //Layer 1 - Image
-        Image(painter = painter, contentDescription = description)
+            //Layer 1 - Image
+            Image(painter = painter, contentDescription = description)
 
-        //Layer 2 - Gradient - wrap in a box to apply modifier
+            //Layer 2 - Gradient - wrap in a box to apply modifier
 
-        Box(
-            modifier = Modifier
-                    .fillMaxSize()
-                    .background(
-                        brush = Brush.verticalGradient(
-                            colors = listOf(
-                                Color.Transparent,
-                                Color.Black
-                            ),
-                            startY = 300f
+            Box(
+                modifier = Modifier
+                        .fillMaxSize()
+                        .background(
+                            brush = Brush.verticalGradient(
+                                colors = listOf(
+                                    Color.Transparent,
+                                    Color.Black
+                                ),
+                                startY = 300f
+                            )
                         )
-                    )
-        )
-        
-        
-        //Layer 3 - Text - align to bottom left corner
-        Box(modifier = Modifier.fillMaxWidth(), contentAlignment = Alignment.BottomStart){
-            
-            Text(text = title)
+            )
+
+
+            //Layer 3 - Text - align to bottom left corner
+            Box(modifier = Modifier.fillMaxWidth(), contentAlignment = Alignment.BottomStart){
+
+                Text(text = title)
+            }
+
+
         }
-        
-        
     }
+
+
 
 }
