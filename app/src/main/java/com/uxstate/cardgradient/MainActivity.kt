259ce00aec40262411c094756622d5a6ca9f5535
Antony <vontonnie@gmail.com>
2022-01-18 20:41:28 +0300

    Style the text

--- app/src/main/java/com/uxstate/cardgradient/MainActivity.kt
+++ app/src/main/java/com/uxstate/cardgradient/MainActivity.kt
@@ -5,10 +5,8 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -17,8 +15,11 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.uxstate.cardgradient.ui.theme.CardGradientTheme
 
 class MainActivity : ComponentActivity() {
@@ -51,11 +52,14 @@ class MainActivity : ComponentActivity() {
 fun ImageCardComposable(painter: Painter, description: String, title: String) {
 
 
-    Card(elevation = 5.dp,modifier = Modifier.width(250.dp)) {
-        Box() {
+    Card(modifier = Modifier.fillMaxWidth(),
+        elevation = 5.dp,
+        shape = RoundedCornerShape(15.dp)
+    ) {
+        Box(Modifier.height(200.dp)) {
 
             //Layer 1 - Image
-            Image(painter = painter, contentDescription = description)
+            Image(painter = painter, contentDescription = description, contentScale = ContentScale.Crop)
 
             //Layer 2 - Gradient - wrap in a box to apply modifier
 
@@ -77,7 +81,7 @@ fun ImageCardComposable(painter: Painter, description: String, title: String) {
             //Layer 3 - Text - align to bottom left corner
             Box(modifier = Modifier.fillMaxWidth(), contentAlignment = Alignment.BottomStart){
 
-                Text(text = title)
+                Text(text = title, style = TextStyle(color = Color.White, fontSize = 16.sp))
             }
 
 
